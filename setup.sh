6f2ba492b1bb5eb9c85541185cd409b0be6e7acd
Peter Geertsema <peterjgeertsema@gmail.com>
2022-04-09 19:10:51 -0400

    .

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
-poetry install
+# poetry install
 wget https://www.adrianbulat.com/downloads/python-fan/s3fd-619a316812.pth
 mv s3fd-619a316812.pth Wav2Lip/face_detection/detection/sfd/s3fd.pth
 wget https://get.station307.com/WhuFlIyLM2d/wav2lip.pth
 mv wav2lip.pth Wav2Lip/checkpoints/
-poetry run python voice/setup.py
+python voice/setup.py
 
