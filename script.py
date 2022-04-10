c65260bd0a50a214434e7cd9c0e4334bdd70c949
Peter Geertsema <peterjgeertsema@gmail.com>
2022-04-09 23:07:48 -0400

    .

--- script.py
+++ script.py
@@ -1,32 +1,29 @@
-from multiprocessing import Process, Queue
-from video.fake_error_connection import play_video, play_music, video_to_frames
+from threading import Thread
+from video.fake_error_connection import play_video, play_music, video_to_frames, generate_rgb_noise
 
 
-def coordinate(queue):
+def coordinate():
+    print("coordinate")
     video_file = "result_voice.mp4"
     audio_file = "result_audio.mp3"
     frames = video_to_frames(video_file)
+    # frames = [generate_rgb_noise((720, 1280, 3)) for i in range(100)]
     print("Frames processing done!")
-    music_queue = Queue()
-    p1 = Process(target=play_music, args=(music_queue,))
-    p1.start()
+    Thread(target=play_music, args=(audio_file,)).start()
+    Thread(target=play_video, args=(frames,)).start()
 
-    video_queue = Queue()
-    p2 = Process(target=play_video, args=(video_queue,))
-    p2.start()
+    # video_queue = Queue()
+    # p2 = Process(target=play_video, args=(video_queue,))
+    # p2.start()
 
-    while True:
-        if not queue.is_empty():
-            queue.get()
-            music_queue.put(audio_file)
-            for frame in frames:
-                video_queue.put(frame)
+    # while True:
+    #     if not queue.empty():
+    #         r = queue.get()
+    #         music_queue.put(audio_file)
+    #         # for frame in frames:
+    #         #     video_queue.put(frame)
+    #         break
 
 
 if __name__ == "__main__":
-    q = Queue()
-    p = Process(target=coordinate, args=(q,))
-    p.start()
-
-    input()
-    q.put("Hello")
+    coordinate()
--- video/fake_error_connection.py
+++ video/fake_error_connection.py
@@ -3,20 +3,20 @@ import time
 import ffmpeg
 import pyvirtualcam
 from pygame import mixer
+import pygame
 import moviepy.editor as mp
-
-mixer.init(devicename='CABLE Input (VB-Audio Virtual Cable)')
 
 
 def generate_rgb_noise(dim: tuple) -> np.ndarray:
     return (np.random.random(dim) * 256).astype(np.uint8)
 
+
 def video_to_frames(path: str) -> np.ndarray:
     out, _ = (
-    ffmpeg
-    .input(path)
-    .output('pipe:', format='rawvideo', pix_fmt='rgb24', vframes=900)
-    .run(capture_stdout=True)
+        ffmpeg
+        .input(path)
+        .output('pipe:', format='rawvideo', pix_fmt='rgb24', vframes=900)
+        .run(capture_stdout=True)
     )
     video = (
         np
@@ -26,22 +26,26 @@ def video_to_frames(path: str) -> np.ndarray:
     return video
 
 
-def play_video(queue):
+def play_video(frames):
     with pyvirtualcam.Camera(width=1280, height=720, fps=25) as cam:
-        frame = generate_rgb_noise((cam.height, cam.width, 3))
-        while True:
-            if not queue.empty():
-                frame = queue.get()
+        for frame in frames:
             cam.send(frame)
             cam.sleep_until_next_frame()
+        # while True:
+        #     if not queue.empty():
+        #         frame = queue.get()
+        #         if frame == "end":
+        #             break
+        #     cam.send(frame)
+        #     cam.sleep_until_next_frame()
 
 
-def play_music(queue):
-    while True:
-        if not queue.empty():
-            file_path = queue.get()
-            mixer.music.load(file_path)
-            mixer.music.play()
+def play_music(file_path):
+    print(file_path)
+    mixer.init(devicename='CABLE Input (VB-Audio Virtual Cable)')
+    mixer.music.load(file_path)
+    mixer.music.play()
+
 
 def write_audio(video_path: str, audio_path: str):
     clip = mp.VideoFileClip(video_path)
